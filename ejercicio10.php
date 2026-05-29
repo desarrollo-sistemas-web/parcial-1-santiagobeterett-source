1f02315a5dfa1a71bbdde7767ecc84647668cf05
santiagobeterett-source <santiago.beterett@gmail.com>
2026-05-28 22:26:52 -0300

    Add files via upload

--- ejercicio10.php
+++ ejercicio10.php
@@ -1,6 +1,7 @@
 <?php
 
-//Ejercicio 10: Crea una función que reciba tantos números como uno quisiera pasar y me devuelva la multiplicación de todos ellos.
+//Ejercicio 10: Crea una función que reciba tantos números como uno quisiera pasar
+//y me devuelva la multiplicación de todos ellos.
 //Complejidad: MEDIA (2 puntos)
 
 // 🧠 Pista: A veces los puntos suspensivos dicen mucho ...$digo ...
--- ejercicio3.php
+++ ejercicio3.php
@@ -1,9 +1,14 @@
 <?php
 
 //Ejercicio 3: Crea una función que tome un número
-//entero como parámetro y devuelva true si el número es par y false si es impar.
+// entero como parámetro y devuelva true si el número es par y false si es impar.
 //Complejidad: MEDIA (2 puntos)
 
-function esPar($num) {
-    if ($num = 0  ) ;
+function esPar($num)
+{
+    if ($num % 2 == 0) {
+       return true;
+    } else {
+        return false;
+    }
 }
